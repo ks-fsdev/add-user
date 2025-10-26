80e45265a48759e55e4eedd5ab14f0c8db5e63a3
Khushi Srivastava <sweetkhushi2000@gmail.com>
2025-10-26 15:56:46 +0530

    first commit

---
+++ app.js
@@ -0,0 +1,9 @@
+const renderUser = () => {
+  let div = document.createElement(div);
+};
+
+const addUser = () => {
+  const btn = document.getElementById("btn");
+  const usersContainer = document.getElementById("usersContainer");
+  renderUser();
+};
